d54d26781f7fbfd5b86fdeb06da375e34ba73fa4
Cooper Lender <cooperlender@Coopers-MacBook-Air.local>
2024-05-13 14:02:06 -0700

    add js files

--- webapp.py
+++ webapp.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, url_for, session, request, jsonify, render_template, flash
 from markupsafe import Markup
-from flask_apscheduler import APScheduler
-from apscheduler.schedulers.background import BackgroundScheduler
+# from flask_apscheduler import APScheduler
+# from apscheduler.schedulers.background import BackgroundScheduler
 from flask_oauthlib.client import OAuth
 from bson.objectid import ObjectId
 
@@ -103,6 +103,20 @@ def renderPage2():
 def get_github_oauth_token():
     return session['github_token']
 
-
+def database():
+    connection_string = os.environ["MONGO_CONNECTION_STRING"]
+    db_name = os.environ["MONGO_DBNAME"]
+    
+    client = pymongo.MongoClient(connection_string)
+    db = client[db_name]
+    collection = db['users'] #1. put the name of your collection in the quotes
+    
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+    
 if __name__ == '__main__':
     app.run()
